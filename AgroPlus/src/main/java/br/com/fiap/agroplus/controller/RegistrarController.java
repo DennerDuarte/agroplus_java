137bde9dae5b60267a23ca707202fd79e4b95db2
DennerDuarte <rm551938@fiap.com.br>
2024-11-03 16:52:50 -0300

    Corrigido a requisição gerada pelo controller de Registro

--- AgroPlus/src/main/java/br/com/fiap/agroplus/controller/RegistrarController.java
+++ AgroPlus/src/main/java/br/com/fiap/agroplus/controller/RegistrarController.java
@@ -3,11 +3,10 @@ package br.com.fiap.agroplus.controller;
 import br.com.fiap.agroplus.entity.Login;
 import br.com.fiap.agroplus.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RegistrarController {
@@ -19,11 +18,14 @@ public class RegistrarController {
     private PasswordEncoder encoder;
 
     @PostMapping("/signup")
-    public String signup(@RequestBody Login login) {
+    public ResponseEntity<String> signup(@RequestBody Login login) {
+        if (repository.findByUsername(login.getUsername()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Usuário já existe");
+        }
+
         login.setPassword(encoder.encode(login.getPassword()));
         repository.save(login);
 
-        return "redirect:/login";
+        return ResponseEntity.status(HttpStatus.CREATED).body("Usuário cadastrado com sucesso");
     }
-
 }
